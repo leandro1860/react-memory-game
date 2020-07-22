2cc530f4366c768783b0731ad26e7e458088ee1e
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-07-22 15:43:12 -0300

    Updates

---
+++ precache-manifest.061822da23e45304726d2dfd1f491469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace433297eef92caaec4be3bc32b9176",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "a4b107e3560386aae2ca",
+    "url": "/react-memory-game/static/css/main.705de421.chunk.css"
+  },
+  {
+    "revision": "447542e3a6fad8426099",
+    "url": "/react-memory-game/static/js/2.2459147a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memory-game/static/js/2.2459147a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b107e3560386aae2ca",
+    "url": "/react-memory-game/static/js/main.a4e8f43c.chunk.js"
+  },
+  {
+    "revision": "f5205ec89729a65ab69d",
+    "url": "/react-memory-game/static/js/runtime-main.e42891cf.js"
+  }
+]);
